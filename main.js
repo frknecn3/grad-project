a89fb93c37dba3f79f72d779585cf8cea9b2804a
frknecn3 <furkanduhannuman@gmail.com>
2026-01-24 01:38:58 +0300

    feat(hero): remade hero section entirely

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm";
 const articleList = document.querySelector('.article-list')
 
+
 let articles = [];
+let showcase = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
 
@@ -12,14 +14,30 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         articles = [...res.data.articles]
 
-        articles = articles.filter((article)=>article.urlToImage).map((article, i) => buildArticle(article))
+        articles = articles.filter((article) => article.urlToImage).map((article, i) => buildArticle(article))
 
         articles.map((article, i) => renderArticle(article))
     }
 
     catch (err) {
-        console.error(err)
+        console.error("err during loading articles:", err)
     }
+
+    try {
+        const res = await axios.get('https://newsapi.org/v2/everything?q=world&apiKey=86a1adfd80e4427d8c2493d5573ea849')
+
+        console.log("sc:", res)
+
+        showcase = res.data.articles.slice(0, 6)
+
+
+    }
+    catch (err) {
+        console.error("err during loading showcase:", err)
+
+    }
+
+    loadShowcase()
 
 })
 
@@ -42,10 +60,10 @@ const buildArticle = (article) => {
                         ${article.title}
                     </h2>
                     <span class="article-details">
-                        ${article.author!==null ? `
+                        ${article.author !== null ? `
                         <address>
                             ${article.author}
-                        </address> -` :''}
+                        </address> -` : ''}
                         
                         <time>${article.publishedAt}</time>
                     </span>
@@ -69,8 +87,45 @@ const renderArticle = (htmlArticle) => {
     articleList.appendChild(htmlArticle)
 }
 
+// showcase kısmını yükleyen fonksiyon
+
+const loadShowcase = () => {
+
+    const displayText = document.querySelector('.hero-text')
+    const displayImage = document.querySelector('.display-image');
+    const secondaryImages = document.querySelectorAll('.secondary-image')
+    secondaryImages.forEach((img, index) => {
+        img.src = showcase[index + 1].urlToImage
+        img.dataset.article = index + 1
+    })
 
 
+    displayText.firstElementChild.innerText = showcase[0].title
+
+    console.log(showcase[0])
+    displayImage.src = showcase[0].urlToImage || './assets/images/notfound.jpg';
+
+    secondaryImages.forEach((img, index) => {
+        img.addEventListener('mouseover', () => {
+            // img.classList.add('hovered')
+
+            let art = showcase[img.dataset.article];
+            displayText.firstElementChild.innerText = art.title
+            displayImage.src = art.urlToImage
+            displayImage.parentElement.href = "amogus"
+        })
+
+        img.addEventListener('mouseleave', ()=>{
+            // img.classList.remove('hovered')
+        })
+    })
+
+}
+
+
+const hoverElement = (el) => {
+
+}
 
 // handle img errors
 
